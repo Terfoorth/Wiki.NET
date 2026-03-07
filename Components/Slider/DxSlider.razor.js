3c0a6729983e5e3ff14e858872e24de974ea28cf
terfoore <terfoore@DSEKTOP_03>
2026-03-07 12:49:55 +0100

    Add BlazorSlider Files for rework and mergin into Wiki.NET App.

---
+++ Components/Slider/DxSlider.razor.js
@@ -0,0 +1,72 @@
+﻿let updateInvokedByServer = false;
+let serverIsProcessing = false;
+let queuedValue = null;
+
+export async function initializeSlider(element, dotNet, sliderState) {
+    return new DevExpress.ui.dxSlider(element, {
+        min: sliderState.minValue,
+        max: sliderState.maxValue,
+        value: sliderState.value,
+        step: sliderState.step === null ? 1 : sliderState.step,
+        showRange: sliderState.showRange,
+        disabled: !sliderState.enabled,
+        valueChangeMode: decapitalize(sliderState.valueChangeMode),
+        label: {
+            visible: sliderState.labelVisible,
+            format(value) {
+                return `${value}%`;
+            },
+            position: decapitalize(sliderState.labelPosition)
+        },
+        tooltip: {
+            enabled: sliderState.tooltipEnabled,
+            format(value) {
+                return `${value}%`;
+            },
+            showMode: decapitalize(sliderState.tooltipShowMode),
+            position: decapitalize(sliderState.tooltipPosition)
+        },
+        onValueChanged({ value }) {
+            if(!updateInvokedByServer) {
+                scheduleServerValueUpdate(dotNet, value);
+            }
+        },
+    });
+}
+
+export async function updateStateFromServer(slider, state) {
+    updateInvokedByServer = true;
+    slider.option("value", state.value);
+    slider.option("min", state.minValue);
+    slider.option("max", state.maxValue);
+    slider.option("step", state.step === null ? 1 : state.step);
+    slider.option("showRange", state.showRange);
+    slider.option("disabled", !state.enabled);
+    slider.option("valueChangeMode", decapitalize(state.valueChangeMode));
+    updateInvokedByServer = false;
+}
+
+function decapitalize(word) {
+    return word[0].toLowerCase() + word.slice(1);
+}
+
+async function scheduleServerValueUpdate(dotNetRef, value) {
+    if(serverIsProcessing) {
+        queuedValue = value;
+        return;
+    }
+
+    serverIsProcessing = true;
+
+    try {
+        await dotNetRef.invokeMethodAsync("UpdateValueFromClient", value);
+    }
+    finally {
+        serverIsProcessing = false;
+        if(queuedValue !== null) {
+            const v = queuedValue;
+            queuedValue = null;
+            scheduleServerValueUpdate(dotNetRef, v);
+        }
+    }
+}
---
+++ Components/Slider/SliderState.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Blazor;
+using System.Text.Json.Serialization;
+
+namespace BlazorSlider.Components.Slider;
+
+public enum SliderValueChangeMode { OnHandleMove, OnHandleRelease }
+
+public enum TooltipShowMode { OnHover, Always }
+
+public class SliderState<T> where T : struct {
+    public T Value { get; set; } 
+    public T? Step { get; set; } 
+    public T MinValue { get; set; } 
+    public T MaxValue { get; set; }
+    public bool ShowRange { get; set; } = true;
+    public bool Enabled { get; set; } = true;
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public SliderValueChangeMode ValueChangeMode { get; set; }
+    public bool LabelVisible { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public VerticalEdge LabelPosition { get; set; } = VerticalEdge.Bottom;
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public VerticalEdge TooltipPosition { get; set; } = VerticalEdge.Top;
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public TooltipShowMode TooltipShowMode { get; set; }
+    public bool TooltipEnabled { get; set; }
+}
